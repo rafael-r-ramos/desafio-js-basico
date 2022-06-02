517910807680b3ed602de5513b50c75e31e08967
Rafael Ramos <rafaelribeiroramos96@gmail.com>
2022-06-02 15:16:08 -0300

    adicionado o arquivo js

---
+++ index.js
@@ -0,0 +1,30 @@
+    function somar(numero1, numero2) {
+         return  numero1 + numero2
+    }
+
+    let resultadoSoma = somar(10,2)
+    console.log('O resultado da soma de 10 + 2 é ' + resultadoSoma)
+
+    function subtrair(numero1, numero2) {
+        return numero1 - numero2
+    }
+
+    let resultadoSubtracao = subtrair(10,2)
+    console.log('O resultado da subtracao 10 - 2 é ' + resultadoSubtracao)
+
+    function multiplicar(numero1, numero2) {
+        return numero1 * numero2
+    }
+
+    let resultadoMultiplicacao = multiplicar(10,2)
+    console.log('o resultado da multiplicação de 10 * 2 é ' + resultadoMultiplicacao)
+
+    function dividir(numero1, numero2) {
+        return numero1 / numero2
+    }
+
+    let resultadoDivisao = dividir(10,2)
+    console.log('o resultado da divisão de 10 / 2 é ' + resultadoDivisao)
+
+
+    
